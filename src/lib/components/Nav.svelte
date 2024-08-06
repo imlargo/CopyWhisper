e1e1131d378b9917a51c7ed86a33677eb426800e
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-08-06 02:14:57 +0000

    eliminar elementos inecesarios del nav

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,24 +1,11 @@
 <nav class="flex py-5 justify-between items-center">
-	<div class="flex basis-1/3 align-center">
+	<div class="flex align-center">
 		<a class="text-2xl select-none font-mono" href="/">
 			<i class="bi bi-pen"></i>
 			<span>CopyWhisper</span>
 		</a>
 	</div>
 
-	<div class="flex basis-1/3">
-		<div
-			class="flex mx-auto px-12 py-1.5 justify-center items-center gap-10 border border-white/10 rounded-full bg-white/5"
-		>
-			<a href="/">Home</a>
-		</div>
-	</div>
-
-	<div class="flex basis-1/3 justify-end items-center gap-7">
-		<button class="bg-violet-600 px-5 py-1 rounded-full text-white font-medium font-mono"
-			>Sign Up</button
-		>
-	</div>
 </nav>
 
 <style lang="scss">
