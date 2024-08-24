4bcfb5941d587133664df31c33e654a4186ce7f4
Juan Carlos Largo <jlargob@unal.edu.co>
2024-08-24 11:25:43 -0500

    chore: agregar redes sociales en barra de navegacion

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,9 +1,37 @@
 <nav class="flex py-5 justify-between items-center">
+
 	<div class="flex align-center">
 		<a class="text-2xl select-none font-mono" href="/">
 			<i class="bi bi-pen"></i>
 			<span>CopyWhisper</span>
 		</a>
+	</div>
+
+	<div class="flex align-center justify-end items-center gap-7">
+		<a
+			class=" transition-all duration-300 hover:scale-110 active:scale-95 text-lg"
+			href="https://github.com/imlargo"
+			target="_blank"
+		>
+			<i class="bi bi-github"></i>
+		</a>
+
+		<a
+			class=" transition-all duration-300 hover:scale-110 active:scale-95 text-lg"
+			href="https://www.instagram.com/imlargo"
+			target="_blank"
+		>
+			<i class="bi bi-instagram"></i>
+		</a>
+
+		<a
+			class=" transition-all duration-300 hover:scale-110 active:scale-95 text-lg"
+			href="https://www.linkedin.com/in/imlargo"
+			target="_blank"
+		>
+			<i class="bi bi-linkedin"></i>
+		</a>
+
 	</div>
 
 </nav>
